a75d2845e0a0f4a91596e73bc6ba3d0dd803ebd6
Eric johnson <ej2586@gmail.com>
2025-12-03 08:25:29 -0800

    fix: configure Vercel for monorepo workspace builds

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     remotePatterns: [new URL('https://s.yimg.com/**')],
   },
+  transpilePackages: [
+    '@ewjdev/anyclick-core',
+    '@ewjdev/anyclick-react',
+    '@ewjdev/anyclick-cursor',
+    '@ewjdev/anyclick-cursor-local',
+    '@ewjdev/anyclick-github',
+  ],
 }
